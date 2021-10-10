2edf26eed6c767aa46c2548dabbe09e3c1e8e739
tamamatsu <2051044h@stu.kobe-u.ac.jp>
2021-10-10 16:38:29 +0900

    Change section1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -8,9 +8,9 @@ function draw() {
   fill(178, 100, 162);
   triangle(12,120,116,120,116,10);
   fill(94, 185, 84);
-  triangle(0,108,106,108,106,0);
+  triangle(12,120,106,108,116,10);
   fill(255);
-  textSize(32);
+  textSize(36);
   textFont("serif");
-  text("46", 68, 100);
+  text("88", 68, 100);
 }
